d60f21c0f5e694143513e4030e4b5120ca143fe1
Lakshya Dhawan <lakshyad1902@gmail.com>
2024-05-16 19:29:44 +0530

    added Factorial.java

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,2 +1,23 @@
 public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        factorialParameterized(n,1);
+        System.out.println(factorialFunctional(n));
+    }
+
+    private static int factorialFunctional(int n) {
+        if(n<2){
+            return 1;
+        }
+        return n * factorialFunctional(n-1);
+    }
+
+    private static void factorialParameterized(int n, int prod) {
+        if(n==0){
+            System.out.println(prod);
+            return;
+        }
+
+        factorialParameterized(n-1, prod*n);
+    }
 }
